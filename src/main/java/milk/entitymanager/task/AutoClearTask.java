795a7a9a511c888bb2c9dd1a11e5f188c1f84a36
milk0417 <milk5590@naver.com>
2016-08-09 00:59:14 +0900

    오토 클리어 관련 수정

--- src/main/java/milk/entitymanager/task/AutoClearTask.java
+++ src/main/java/milk/entitymanager/task/AutoClearTask.java
@@ -1,8 +1,10 @@
 package milk.entitymanager.task;
 
 import cn.nukkit.Server;
-import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.item.EntityItem;
+import cn.nukkit.entity.projectile.EntityProjectile;
 import milk.entitymanager.EntityManager;
+import milk.pureentities.entity.BaseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +14,29 @@ public class AutoClearTask implements Runnable{
     @Override
     public void run(){
         EntityManager owner = (EntityManager) Server.getInstance().getPluginManager().getPlugin("EntityManager");
-        List list = owner.getData("autoclear.entitites", new ArrayList<String>(){{
-            add("Projectile");
-            add("DroppedItem");
+        List<String> list = owner.getData("autoclear.entitites", new ArrayList<String>(){{
+            add("EntityProjectile");
+            add("EntityItem");
         }});
-        Server.getInstance().getLevels().forEach((id, level) -> {
-            for(Entity entity : level.getEntities()){
-                if(list.contains(entity.getClass().getSimpleName())){
-                    entity.close();
-                }
+        int index = 0;
+        Class[] type = new Class[list.size()];
+        for(String k : list){
+            switch(k){
+                case "Projectile":
+                case "EntityProjectile":
+                    type[index++] = EntityProjectile.class;
+                    break;
+                case "Item":
+                case "EntityItem":
+                case "DroppedItem":
+                    type[index++] = EntityItem.class;
+                    break;
+                case "BaseEntity":
+                    type[index++] = BaseEntity.class;
+                    break;
             }
-        });
+        }
+        Server.getInstance().getLevels().forEach((id, level) -> EntityManager.clear(type, level));
     }
 
 }
